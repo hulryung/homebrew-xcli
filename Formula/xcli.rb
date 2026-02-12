f0e2b83f029082916ba3c67cadf0e36290b4de44
Daekeun Kang <dkkang@huconn.com>
2026-02-12 11:20:52 +0900

    Add placeholder formula for xcli

---
+++ Formula/xcli.rb
@@ -0,0 +1,34 @@
+class Xcli < Formula
+  desc "X (Twitter) API CLI tool"
+  homepage "https://github.com/hulryung/xcli"
+  version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "xcli"
+  end
+
+  test do
+    assert_match "xcli", shell_output("#{bin}/xcli --help")
+  end
+end
