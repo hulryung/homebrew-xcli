32ce5fc974591b5b665fd2b37335d7c7a30cb99d
Daekeun Kang <dkkang@huconn.com>
2026-02-12 11:36:27 +0900

    Fix SHA256 checksums for v1.0.0

--- Formula/xcli.rb
+++ Formula/xcli.rb
@@ -7,20 +7,20 @@ class Xcli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-x86_64-apple-darwin.tar.gz"
-      sha256 "Not"
+      sha256 "32312e72e121651532428b55a1018327267c6b365cdb7b96e28a1b0ce1b02f6c"
     else
       url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-aarch64-apple-darwin.tar.gz"
-      sha256 "Not"
+      sha256 "90113977a04478064c30460035e57373f0ccf4dd2dd0f16c976e79bfcfd9530c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "Not"
+      sha256 "55916fcd6d19e94b46fdc9aa9c51de152538d43052ae967a4d0b494d4c559ad9"
     else
       url "https://github.com/hulryung/xcli/releases/download/v1.0.0/xcli-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "Not"
+      sha256 "363639eddbbe6254f295ac9ad24b8fefa6c0a820ba788b11cc69f3d0f9daa78e"
     end
   end
 
